0cfab0feecb173f384cf395b6b6667f05d04df0d
Ramandeep-Saini <47928294+Ramandeep-Saini@users.noreply.github.com>
2022-03-16 09:50:26 -0400

    Add files via upload

---
+++ plurality.c
@@ -0,0 +1,102 @@
+#include <cs50.h>
+#include <stdio.h>
+#include <string.h>
+
+// Max number of candidates
+#define MAX 9
+
+// Candidates have name and vote count
+typedef struct
+{
+    string name;
+    int votes;
+}
+candidate;
+
+// Array of candidates
+candidate candidates[MAX];
+
+// Number of candidates
+int candidate_count;
+
+// Function prototypes
+bool vote(string name);
+void print_winner(void);
+
+int main(int argc, string argv[])
+{
+    // Check for invalid usage
+    if (argc < 2)
+    {
+        printf("Usage: plurality [candidate ...]\n");
+        return 1;
+    }
+
+    // Populate array of candidates
+    candidate_count = argc - 1;
+    if (candidate_count > MAX)
+    {
+        printf("Maximum number of candidates is %i\n", MAX);
+        return 2;
+    }
+    for (int i = 0; i < candidate_count; i++)
+    {
+        candidates[i].name = argv[i + 1];
+        candidates[i].votes = 0;
+    }
+
+    int voter_count = get_int("Number of voters: ");
+
+    // Loop over all voters
+    for (int i = 0; i < voter_count; i++)
+    {
+        string name = get_string("Vote: ");
+
+        // Check for invalid vote
+        if (!vote(name))
+        {
+            printf("Invalid vote.\n");
+        }
+    }
+
+    // Display winner of election
+    print_winner();
+}
+
+// Update vote totals given a new vote
+bool vote(string name)
+{
+//loop to compare each candidate's name that was voted for with those in the array
+    for (int i = 0; i < candidate_count; i++)
+    {
+        if (strcmp(candidates[i].name, name) == 0)
+        {
+            candidates[i].votes++;
+            return true;
+        }
+    }
+    return false;
+}
+
+// Print the winner (or winners) of the election
+void print_winner(void)
+{
+    int winner = 0;
+    string winname;
+//loop that keeps track of the greatest number of votes using 0 as a starting point. Candidate name with the most votes is stored in winname
+    for (int i = 0; i < candidate_count; i++)
+        if (candidates[i].votes > winner)
+        {
+            winner = candidates[i].votes;
+            winname = candidates[i].name;
+        }
+//checks if candidate is tied with the winner, and if so, prints all names
+        else if (candidates[i].votes == winner)
+        {
+            printf("%s\n", candidates[i].name);
+        }
+//prints winner's name if only one winner
+    printf("%s\n", winname);
+    return;
+}
+
